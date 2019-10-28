d72309e5ed132eaf47022d026a66df1e1cae7b99
zhuzichu <coffee.zhu@jollycorp.com>
2019-10-28 14:37:11 +0800

    update

--- app/src/main/java/com/zhuzichu/android/mvvm/base/BaseActivity.kt
+++ app/src/main/java/com/zhuzichu/android/mvvm/base/BaseActivity.kt
@@ -37,9 +37,9 @@ abstract class BaseActivity : DaggerAppCompatActivity() {
     private fun initArgument(fragment: NavHostFragment) {
         var bundle = fragment.arguments
         if (bundle != null) {
-            bundle.putParcelable(KEY_ARGUMENT, intent.getParcelableExtra(KEY_ARGUMENT))
+            bundle.putParcelable(KEY_ARGUMENT, intent.getParcelableExtra(KEY_ARGUMENT)?:ArgumentDefault())
         } else {
-            bundle = bundleOf(KEY_ARGUMENT to intent.getParcelableExtra(KEY_ARGUMENT))
+            bundle = bundleOf(KEY_ARGUMENT to (intent.getParcelableExtra(KEY_ARGUMENT)?:ArgumentDefault()))
         }
         fragment.arguments = bundle
     }
